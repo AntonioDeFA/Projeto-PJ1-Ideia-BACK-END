cb76659240cc4a0b025ad4180f608702a773a4aa
Nycolas R. Alves <nycolasramon3@gmail.com>
2021-09-30 17:24:26 -0300

    Commit inicial - preparo do ambiente
    Adicionadas dependências e banco de dados já está sendo criado

---
+++ projetoideia/src/main/java/com/ideia/projetoideia/ProjetoideiaApplication.java
@@ -0,0 +1,13 @@
+package com.ideia.projetoideia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoideiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoideiaApplication.class, args);
+	}
+
+}
---
+++ projetoideia/src/test/java/com/ideia/projetoideia/ProjetoideiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideia.projetoideia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoideiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
