dad1ed33b32c49b782f188af3c6580ee7f4675cf
Gabryel Alexandre <gabryel2348369jg@gmail.com>
2022-03-18 20:50:22 -0300

    Cucumber Integration

---
+++ projetoideia/src/test/java/runners/RunnerTest.java
@@ -0,0 +1,11 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features" , dryRun = false, glue = "stepsDefinitions")
+public class RunnerTest {
+}
---
+++ projetoideia/src/test/resources/features/test.feature
@@ -0,0 +1,8 @@
+#language: pt
+#enconding: UTF-8
+
+
+Funcionalidade: Teste Cucumber
+
+	Cenario: Test
+		Dado que eu printe "Antonio"
