01acf7a996162c7aabe0b20c8622a14b8b4e00bf
Antônio Amorim <antonio.amorim.1119@gmail.com>
2022-04-11 22:25:18 -0300

    Revisão de UML e adição no mapeamento na entidade LeanCanvas

--- projetoideia/src/main/java/com/ideia/projetoideia/model/LeanCanvas.java
+++ projetoideia/src/main/java/com/ideia/projetoideia/model/LeanCanvas.java
@@ -2,6 +2,8 @@ package com.ideia.projetoideia.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -44,6 +46,7 @@ public class LeanCanvas {
 	@Column(name = "fontes_de_receita")
 	private String fontesDeReceita;
 
+	@Enumerated(EnumType.STRING)
 	private EtapaArtefatoPitch etapaSolucaoCanvas;
 
 	@ManyToOne
