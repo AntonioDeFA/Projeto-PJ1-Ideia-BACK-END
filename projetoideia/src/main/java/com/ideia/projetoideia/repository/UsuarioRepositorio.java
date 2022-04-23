11e1d69c03ca9d969105389021d3756d5f53be37
Gabryel Alexandre <gabryel2348369jg@gmail.com>
2022-04-23 01:50:26 -0300

    Alteração no tempo de token

--- projetoideia/src/main/java/com/ideia/projetoideia/repository/UsuarioRepositorio.java
+++ projetoideia/src/main/java/com/ideia/projetoideia/repository/UsuarioRepositorio.java
@@ -3,7 +3,6 @@ package com.ideia.projetoideia.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.ideia.projetoideia.model.Usuario;
 
