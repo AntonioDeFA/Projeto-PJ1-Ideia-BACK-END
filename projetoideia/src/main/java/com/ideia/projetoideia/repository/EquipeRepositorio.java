fde0f1423a2de37ac1a011544a0bc51d797415fb
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2022-04-25 01:14:33 -0300

    Atualizando o service e repository de equipe

--- projetoideia/src/main/java/com/ideia/projetoideia/repository/EquipeRepositorio.java
+++ projetoideia/src/main/java/com/ideia/projetoideia/repository/EquipeRepositorio.java
@@ -12,6 +12,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.ideia.projetoideia.model.Equipe;
 import com.ideia.projetoideia.model.Usuario;
+import com.ideia.projetoideia.model.dto.UsuarioDto;
 
 public interface EquipeRepositorio extends JpaRepository<Equipe, Integer> {
 
@@ -25,6 +26,10 @@ public interface EquipeRepositorio extends JpaRepository<Equipe, Integer> {
 	@Query(value = "select * from tb_equipe where token = :token", nativeQuery = true)
 	public Optional<Equipe> consultarEquipePorToken(@Param("token") String token);
 	
-	public List<Equipe> findByLider(Usuario lider);
+	@Query(value = "select count(*) from tb_equipe tbe join tb_competicao tbc on tbc.id = tbe.competicao_fk where tbe.lider_fk = :id_usuario and tbc.id = :id_competicao and tbc.organizador_fk != :id_usuario", nativeQuery = true)
+	public int validarUsuarioLiderEOrganizador(@Param("id_usuario") int idUsuario, @Param("id_competicao") int idCompeticao);
 
+	@Query(value = "select count(*) from tb_usuario_membro_comum tbumc join tb_equipe tbe on tbumc.equipe_fk = tbe.id where tbumc.email in(:lista) and tbe.competicao_fk = :id_competicao", nativeQuery = true)
+	public int validarMembrosDeUmaEquipeEmUmaCompeticao(@Param("lista") List<String> emails, @Param("id_competicao") int idCompeticao);
+	
 }
