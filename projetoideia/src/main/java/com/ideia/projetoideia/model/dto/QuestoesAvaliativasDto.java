f233dff1c13cd88b7c42fa89bbd01e5644fca509
Antônio Amorim <antonio.amorim.1119@gmail.com>
2022-05-19 09:16:59 -0300

    Correção de Bug de enumeração de questão avaliativa nulo

--- projetoideia/src/main/java/com/ideia/projetoideia/model/dto/QuestoesAvaliativasDto.java
+++ projetoideia/src/main/java/com/ideia/projetoideia/model/dto/QuestoesAvaliativasDto.java
@@ -7,27 +7,23 @@ import lombok.Data;
 
 @Data
 public class QuestoesAvaliativasDto {
-	
-	
+
 	private Integer id;
-	
-	
+
 	private Float notaMax;
-	
+
 	private String questao;
-	
+
 	private Integer enumeracao;
-	
+
 	private TipoQuestaoAvaliativa tipoQuestaoAvaliativa;
-	
-	
-	
+
 	public QuestoesAvaliativasDto(QuestaoAvaliativa questaoAvaliativa) {
 		this.id = questaoAvaliativa.getId();
 		this.notaMax = questaoAvaliativa.getNotaMax();
 		this.questao = questaoAvaliativa.getQuestao();
 		this.tipoQuestaoAvaliativa = questaoAvaliativa.getTipoQuestaoAvaliativa();
+		this.enumeracao = questaoAvaliativa.getEnumeracao();
 	}
-	
 
 }
