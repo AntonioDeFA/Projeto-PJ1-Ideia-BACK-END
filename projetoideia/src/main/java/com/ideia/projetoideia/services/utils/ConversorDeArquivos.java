eab02cd8b8e6c9dc65e7271587cc4d8f66ad0653
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2022-05-19 18:55:35 -0300

    Create ConversorDeArquivos.java

---
+++ projetoideia/src/main/java/com/ideia/projetoideia/services/utils/ConversorDeArquivos.java
@@ -0,0 +1,99 @@
+package com.ideia.projetoideia.services.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Base64;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import com.ideia.projetoideia.model.MaterialEstudo;
+import com.ideia.projetoideia.model.enums.TipoMaterialEstudo;
+
+public class ConversorDeArquivos {
+
+	public static String converterStringParaArquivo(String file, Integer idCompeticao) throws Exception{
+		
+		try {
+			
+			File diretorio = new File("regulamentos");
+			
+			if(!diretorio.exists()) {
+				diretorio.mkdirs();
+			}
+			
+			File arquivo = new File ("regulamentos/regutamento"+idCompeticao+".pdf");
+			FileOutputStream fos = new FileOutputStream (arquivo);
+			
+			byte[] array = Base64.getDecoder().decode(file);
+			
+			System.out.println("Regulamento: "+array);
+			System.out.println("Tamanho: "+array.length);
+			fos.write (array);
+			fos.close(); 
+			
+			System.out.println(arquivo.getAbsolutePath());
+		    
+			return arquivo.getAbsolutePath();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			throw new Exception("Erro ao gerar e salvar arquivo");
+		}
+	    	    
+	}
+	
+	public static void converterStringParaArquivo(List<MaterialEstudo> materiais, Integer idCompeticao) throws Exception{
+		
+		try {
+			
+			File diretorio = new File("materiais");
+			
+			if(!diretorio.exists()) {
+				diretorio.mkdirs();
+			}
+			
+			File diretorioCompeticao = new File("materiais/competicao_"+idCompeticao);
+			
+			if(!diretorioCompeticao.exists()) {
+				diretorioCompeticao.mkdirs();
+			}
+			
+			for(MaterialEstudo material: materiais) {
+				
+				if(material.getTipoMaterialEstudo() != TipoMaterialEstudo.LINK) {
+					
+					byte[] array = Base64.getDecoder().decode(material.getArquivoEstudo());
+					
+					if(material.getTipoMaterialEstudo() == TipoMaterialEstudo.PDF) {
+						
+						File arquivo = new File ("materiais/competicao_"+idCompeticao+"material"+RandomStringUtils.randomAlphanumeric(10).toUpperCase()+".pdf");
+						FileOutputStream fos = new FileOutputStream (arquivo);
+						
+						fos.write (array);
+						fos.close(); 
+						
+					}else {
+						
+						File arquivo = new File ("materiais/competicao_"+idCompeticao+"material"+RandomStringUtils.randomAlphanumeric(10).toUpperCase()+".pdf");
+						FileOutputStream fos = new FileOutputStream (arquivo);
+						
+						fos.write (array);
+						fos.close(); 
+					}
+					
+					
+				}
+				
+				
+			}
+			
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			throw new Exception("Erro ao gerar e salvar arquivo");
+		}
+	    	    
+	}
+}
