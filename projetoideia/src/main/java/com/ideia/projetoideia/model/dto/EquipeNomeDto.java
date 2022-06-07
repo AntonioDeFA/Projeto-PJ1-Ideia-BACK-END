37304e7b702359db57ed8060e77c6401c27aab48
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2022-06-07 16:20:26 -0300

    Atualização da EquipeNomeDto e da EquipeService

--- projetoideia/src/main/java/com/ideia/projetoideia/model/dto/EquipeNomeDto.java
+++ projetoideia/src/main/java/com/ideia/projetoideia/model/dto/EquipeNomeDto.java
@@ -1,5 +1,7 @@
 package com.ideia.projetoideia.model.dto;
 
+import com.ideia.projetoideia.model.Usuario;
+
 import lombok.Data;
 
 @Data
@@ -8,10 +10,19 @@ public class EquipeNomeDto {
 	private Integer id;
 	
 	private String nome;
+	
+	private String consultor;
 
-	public EquipeNomeDto(Integer id, String nome) {
+	public EquipeNomeDto(Integer id, String nome, Usuario consultor) {
 		this.id = id;
 		this.nome = nome;
+		
+		if (consultor != null) {
+			this.consultor = consultor.getNomeUsuario();
+		} else {
+			this.consultor = null;
+		}
+		
 	}
 	
 }
