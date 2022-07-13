f78a9e8ed38a3fcb3cc08f6871432eeb0e09e83f
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2022-07-13 18:44:34 -0300

    Create VersaoPitchDeckDto.java

---
+++ projetoideia/src/main/java/com/ideia/projetoideia/model/dto/VersaoPitchDeckDto.java
@@ -0,0 +1,42 @@
+package com.ideia.projetoideia.model.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ideia.projetoideia.model.FeedbackAvaliativo;
+import com.ideia.projetoideia.model.Pitch;
+
+import lombok.Data;
+
+@Data
+public class VersaoPitchDeckDto {
+	
+	private LocalDateTime data;
+	private String arquivoPitchDeck;
+	private String tipo;
+	private List<FeedbackSugestaoDto> feedbacksAvaliativos;
+	
+	public VersaoPitchDeckDto(Pitch pitch, List<FeedbackAvaliativo> feedbacksAvaliativos) {
+		
+		this.feedbacksAvaliativos = new ArrayList<FeedbackSugestaoDto>();
+		this.data = pitch.getDataCriacao();
+		
+		String tipo = "ARQUIVO";
+		String arquivo = pitch.getPitchDeck();
+		
+		if(pitch.getPitchDeck() == null) {
+			tipo = "VIDEO";
+			arquivo = pitch.getVideo();
+		}
+		
+		this.arquivoPitchDeck = arquivo;
+		this.tipo = tipo;
+		
+		for(FeedbackAvaliativo feedback: feedbacksAvaliativos) {
+			this.feedbacksAvaliativos.add(new FeedbackSugestaoDto( feedback.getTipoFeedback(), feedback.getSugestao()));
+		}
+	}
+
+}
