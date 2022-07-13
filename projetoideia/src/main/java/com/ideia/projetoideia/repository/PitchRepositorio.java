e7db4efc4f8fc0ea59cfbac1a8369c9eae594068
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2022-07-13 18:44:51 -0300

    Update PitchRepositorio.java

--- projetoideia/src/main/java/com/ideia/projetoideia/repository/PitchRepositorio.java
+++ projetoideia/src/main/java/com/ideia/projetoideia/repository/PitchRepositorio.java
@@ -1,9 +1,11 @@
 package com.ideia.projetoideia.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ideia.projetoideia.model.Equipe;
 import com.ideia.projetoideia.model.Pitch;
@@ -15,5 +17,11 @@ public interface PitchRepositorio  extends JpaRepository<Pitch, Integer> {
 	@Query(value = "SELECT * FROM db_ideia.tb_pitch as c where c.equipe_fk = ?1 and c.etapa_avaliacao_video = ?2"
 			, nativeQuery = true)
 	public Pitch findByIdEquipeEEtapaList(Integer idEquipe , String etapaAvaliacaoVideo);
+	
+	@Query(value = "select * from tb_pitch where equipe_fk = :id order by data desc limit 1", nativeQuery = true)
+	public Optional<Pitch> findByIdEquipe(@Param ("id") Integer id);
+	
+	@Query(value = "select * from tb_pitch where equipe_fk = :id and etapa_avaliacao_video = 'AVALIADO_CONSULTOR' order by data desc;", nativeQuery = true)
+	public List<Pitch> findByIdEquipeFeedbacksPitch(@Param ("id") Integer id);
 	
 }
